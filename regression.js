10add97e18e4f40bb4f8f83c0f39425f3da3d5e2
Sachin MM <109701268+Sachinsatya@users.noreply.github.com>
2023-12-19 22:52:05 +0530

    Create regression.js

---
+++ regression.js
@@ -0,0 +1,8 @@
+// regression.js
+
+// Simple linear regression function
+function simpleLinearRegression(x) {
+    // Replace this with your actual regression model
+    // For simplicity, let's assume a basic model: y = 2x + 1
+    return 2 * x + 1;
+}
